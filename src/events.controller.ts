06b41418fda863cc77046a2b04ca031f9859bdf8
mgbld <112904350+mgbld@users.noreply.github.com>
2023-05-30 13:40:15 -0600

    Create and register Events controller

---
+++ src/events.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('/events')
+export class EventsController {
+  @Get()
+  findAll() {
+    return [
+      { id: 1, name: 'First Event' },
+      { id: 2, name: 'Second Event' },
+    ];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return { id: 1, name: 'First Event' };
+  }
+
+  // If JSON comes as input, it would return a JS object.
+  @Post()
+  create(@Body() input) {
+    return input;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() input) {
+    return id;
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id) {}
+}
