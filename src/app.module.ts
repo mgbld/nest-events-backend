2610e2791a123836cab767eca7e9235308bc85ad
mgbld <112904350+mgbld@users.noreply.github.com>
2023-05-30 17:27:31 -0600

    Create and integrate entities with typorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
 
 @Module({
   // Static module method to pass some parameters
@@ -14,6 +15,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'example',
       database: 'nest-events',
+      entities: [Event],
+      synchronize: true,
     }),
   ],
   controllers: [AppController, EventsController],
