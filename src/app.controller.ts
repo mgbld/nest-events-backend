1e763561a46631cde5631ca5be0a86be2ceaf234
mgbld <112904350+mgbld@users.noreply.github.com>
2023-05-30 13:39:42 -0600

    Create route on at toplevel with path /bye

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,14 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Top level path
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/bye')
+  getBye(): string {
+    return 'Bye!';
   }
 }
