01c145c28cdd25c99bd56a6e887d568e1bcfa628
mgbld <112904350+mgbld@users.noreply.github.com>
2023-05-30 14:17:47 -0600

    Create Event DTO model to handle Event controllers payload Implement missing events controller endpoints logic

---
+++ src/event.entity.ts
@@ -0,0 +1,8 @@
+// Lets represent an event
+export class Event {
+  id: number;
+  name: string;
+  description: string;
+  when: Date;
+  address: string;
+}
--- src/events.controller.ts
+++ src/events.controller.ts
@@ -9,34 +9,55 @@ import {
   Post,
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('/events')
 export class EventsController {
+  private events: Event[] = [];
+
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First Event' },
-      { id: 2, name: 'Second Event' },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return { id: 1, name: 'First Event' };
+    return this.events.find((event) => event.id === parseInt(id));
   }
 
   // If JSON comes as input, it would return a JS object.
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input;
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1,
+    };
+    this.events.push(event);
+    return event;
   }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() input) {
-    return id;
+  update(@Param('id') id, @Body() input: UpdateEventDto) {
+    const index = this.events.findIndex((event) => {
+      return event.id === parseInt(id);
+    });
+
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when,
+    };
+
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id') id) {}
+  remove(@Param('id') id) {
+    this.events = this.events.filter((event) => {
+      return event.id !== parseInt(id);
+    });
+  }
 }
